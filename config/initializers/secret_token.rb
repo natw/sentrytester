2c336bfa029b414d47a54aeaa7969a7640036211
Nat Williams <nwilliams@leapfrogonline.com>
2013-09-18 13:42:05 -0500

    fresh rails app (w/ raven req)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sentrytester::Application.config.secret_key_base = '589ac7f3956bb6356e489db8b9b70499af9e666b946767394e29ee9e2016669dc48c29dd709f255b2c1a66893052670c84e8ed2ea27ec34cc8bcf1f1c53db018'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sentrytester::Application.config.session_store :cookie_store, key: '_sentrytester_session'
