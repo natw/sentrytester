492766fe129ed1d84428a87dc944959a8144ab1e
Nat Williams <nwilliams@leapfrogonline.com>
2013-09-18 13:46:53 -0500

    generate a controller

---
+++ app/controllers/things_controller.rb
@@ -0,0 +1,5 @@
+class ThingsController < ApplicationController
+  def index
+    1/0
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sentrytester::Application.routes.draw do
+  root 'things#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
